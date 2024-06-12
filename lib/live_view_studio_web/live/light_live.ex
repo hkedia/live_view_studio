add6f31961007f9080a0b7bd6bcc74f8195d4a76
Harsh Kedia <hkedia@hey.com>
2024-06-12 19:46:12 +0530

    light live module

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,53 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, brightness: 10)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Front Porch Light</h1>
+    <div id="light">
+      <div class="meter">
+        <span style={"width: #{@brightness}%"}>
+          <%= @brightness %>%
+        </span>
+      </div>
+      <button phx-click="off">
+        <img src="/images/light-off.svg" alt="off button" />
+      </button>
+      <button phx-click="down">
+        <img src="/images/down.svg" alt="off button" />
+      </button>
+      <button phx-click="up">
+        <img src="/images/up.svg" alt="off button" />
+      </button>
+      <button phx-click="on">
+        <img src="/images/light-on.svg" alt="on button" />
+      </button>
+    </div>
+    """
+  end
+
+  def handle_event("on", _unsigned_params, socket) do
+    socket = assign(socket, brightness: 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _unsigned_params, socket) do
+    socket = update(socket, :brightness, &min(&1 + 10, 100))
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _unsigned_params, socket) do
+    socket = update(socket, :brightness, &max(&1 - 10, 0))
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _unsigned_params, socket) do
+    socket = assign(socket, brightness: 0)
+    {:noreply, socket}
+  end
+end
