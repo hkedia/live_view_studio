f8d9c90e8486e55105a7c8529b86d708d2c56c25
Harsh Kedia <hkedia@hey.com>
2024-06-12 19:53:58 +0530

    add a botton for setting random brightness

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -19,10 +19,13 @@ defmodule LiveViewStudioWeb.LightLive do
         <img src="/images/light-off.svg" alt="off button" />
       </button>
       <button phx-click="down">
-        <img src="/images/down.svg" alt="off button" />
+        <img src="/images/down.svg" alt="down button" />
+      </button>
+      <button phx-click="random">
+        <img src="/images/fire.svg" alt="random button" />
       </button>
       <button phx-click="up">
-        <img src="/images/up.svg" alt="off button" />
+        <img src="/images/up.svg" alt="up button" />
       </button>
       <button phx-click="on">
         <img src="/images/light-on.svg" alt="on button" />
@@ -38,6 +41,12 @@ defmodule LiveViewStudioWeb.LightLive do
 
   def handle_event("up", _unsigned_params, socket) do
     socket = update(socket, :brightness, &min(&1 + 10, 100))
+    {:noreply, socket}
+  end
+
+  def handle_event("random", _unsigned_params, socket) do
+    brightness = Enum.random(1..100)
+    socket = assign(socket, :brightness, brightness)
     {:noreply, socket}
   end
 
