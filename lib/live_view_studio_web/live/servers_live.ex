7dc2e194df3f3e3ced9b2e3bed5dae07d37de10b
Harsh Kedia <hkedia@hey.com>
2024-06-16 04:58:21 +0530

    live patch and live navigate

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -37,7 +37,7 @@ defmodule LiveViewStudioWeb.ServersLive do
         <div class="nav">
           <.link
             :for={server <- @servers}
-            patch={~p"/servers?#{[id: server]}"}
+            patch={~p"/servers/#{server}"}
             class={if server == @selected_server, do: "selected"}
           >
             <span class={server.status}></span>
@@ -53,37 +53,43 @@ defmodule LiveViewStudioWeb.ServersLive do
       </div>
       <div class="main">
         <div class="wrapper">
-          <div class="server">
-            <div class="header">
-              <h2><%= @selected_server.name %></h2>
-              <span class={@selected_server.status}>
-                <%= @selected_server.status %>
-              </span>
-            </div>
-            <div class="body">
-              <div class="row">
-                <span>
-                  <%= @selected_server.deploy_count %> deploys
-                </span>
-                <span>
-                  <%= @selected_server.size %> MB
-                </span>
-                <span>
-                  <%= @selected_server.framework %>
-                </span>
-              </div>
-              <h3>Last Commit Message:</h3>
-              <blockquote>
-                <%= @selected_server.last_commit_message %>
-              </blockquote>
-            </div>
-          </div>
+          <.server server={@selected_server} />
           <div class="links">
             <.link navigate={~p"/light"}>
               Adjust Lights
             </.link>
           </div>
         </div>
+      </div>
+    </div>
+    """
+  end
+
+  def server(assigns) do
+    ~H"""
+    <div class="server">
+      <div class="header">
+        <h2><%= @server.name %></h2>
+        <span class={@server.status}>
+          <%= @server.status %>
+        </span>
+      </div>
+      <div class="body">
+        <div class="row">
+          <span>
+            <%= @server.deploy_count %> deploys
+          </span>
+          <span>
+            <%= @server.size %> MB
+          </span>
+          <span>
+            <%= @server.framework %>
+          </span>
+        </div>
+        <h3>Last Commit Message:</h3>
+        <blockquote>
+          <%= @server.last_commit_message %>
+        </blockquote>
       </div>
     </div>
     """
