197bc3708617716f80953c99620dcd747445063f
Harsh Kedia <hkedia@hey.com>
2024-06-18 01:55:13 +0530

    using js hooks

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,12 +21,39 @@ import "phoenix_html"
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import flatpickr from "../vendor/flatpickr"
+let Hooks = {}
+
+Hooks.Calendar = {
+  mounted() {
+    this.pickr = flatpickr(this.el, {
+      inline: true,
+      mode: "range",
+      showMonths: 2,
+      onChange: (selectedDates) => {
+        if (selectedDates.length != 2) return
+        this.pushEvent("dates-picked", selectedDates)
+      }
+    })
+    this.handleEvent("add-unavailable-dates", (dates) => {
+      this.pickr.set("disable", [dates, ...this.pickr.config.disable])
+    })
+
+    this.pushEvent("unavailable-dates", {}, (reply, ref) => {
+      this.pickr.set("disable", reply.dates)
+    })
+  },
+  destroyed() {
+    this.pickr.destroy()
+  }
+}
 
 let csrfToken =
   document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
 let liveSocket = new LiveSocket("/live", Socket, {
-  params: { _csrf_token: csrfToken }
+  params: { _csrf_token: csrfToken },
+  hooks: Hooks
 });
 
 // Show progress bar on live navigation and form submits
