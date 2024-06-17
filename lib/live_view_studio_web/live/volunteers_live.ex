4355ae931a67247bf94f0b560c0d528c1a01de08
Harsh Kedia <hkedia@hey.com>
2024-06-18 00:10:12 +0530

    use js commands for transition animations

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -85,15 +85,14 @@ defmodule LiveViewStudioWeb.VolunteersLive do
         <%= @volunteer.phone %>
       </div>
       <div class="status">
-        <button phx-click="toggle-status" phx-value-id={@volunteer.id}>
+        <button phx-click={toggle_volunteer_status(@volunteer, @id)}>
           <%= if @volunteer.checked_out,
             do: "Check In",
             else: "Check Out" %>
         </button>
         <.link
           class="delete"
-          phx-click="delete"
-          phx-value-id={@volunteer.id}
+          phx-click={delete_volunteer_row(@volunteer, @id)}
           data-confirm="Are you sure?"
         >
           <.icon name="hero-trash-solid" />
@@ -101,5 +100,19 @@ defmodule LiveViewStudioWeb.VolunteersLive do
       </div>
     </div>
     """
+  end
+
+  def toggle_volunteer_status(volunteer, id) do
+    JS.push("toggle-status", value: %{id: volunteer.id})
+    |> JS.transition("shake", to: "##{id}", time: 500)
+  end
+
+  def delete_volunteer_row(volunteer, id) do
+    JS.push("delete", value: %{id: volunteer.id})
+    |> JS.transition(
+      "ease duration-1000 scale-150",
+      to: "##{id}",
+      time: 500
+    )
   end
 end
